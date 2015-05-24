1b77033bdc5cc757a12eee0da75ca455516cd061
nirav patel <npatel@xactlycorp.com>
2015-05-24 13:50:10 -0700

    spring bootify - first pahse

--- src/main/java/com/np/trackserver/server/TrackServer.java
+++ src/main/java/com/np/trackserver/server/TrackServer.java
@@ -99,7 +99,7 @@ public class TrackServer implements InitializingBean {
 		logger.info("Track server stopped!!");
 	}
 	
-	public static void main(String[] args) throws Exception
+	public static void main2(String[] args) throws Exception
 	{
 		
 		try {
